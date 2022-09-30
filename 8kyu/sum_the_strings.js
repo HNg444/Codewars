36424283efba29a85c19b80df19350ff306b9928
hassanng-dev <hassanngdev@gmail.com>
2022-09-30 13:16:03 -0400

    Sum the strings problem and solution

---
+++ 8kyu/sum_the_strings.js
@@ -0,0 +1,11 @@
+//Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+//Example: (Input1, Input2 -->Output)
+
+
+function sumStr(a,b) {
+  let num1 = Number(a);
+  let num2 = Number(b);
+  
+  return String(num1 + num2);
+}
